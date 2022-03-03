cce98febea270e4061cb76c517f3f9bddeea2388
rijabaig <90227392+rijabaig@users.noreply.github.com>
2022-03-03 14:26:16 -0500

    Create Director.java

---
+++ CarBuilder/Director.java
@@ -0,0 +1,43 @@
+package csci2020;
+//director classs
+public class Director {
+
+    private carbuilder builder;
+
+    public Director(final carbuilder builder) {
+        this.builder = builder;
+    }
+
+    public cars constructSUV() {
+        return builder.setVehicletype("SUV")
+                .setColor("Red")
+                .setYear(2005)
+                .setFueltype("Unleaded Gas")
+                .build();
+    }
+
+    public cars constructSedan() {
+        return builder.setVehicletype("Sedan")
+                .setColor("Blue")
+                .setYear(2010)
+                .setFueltype("Regular Gas")
+                .build();
+    }
+
+    public cars constructCoupe() {
+        return builder.setVehicletype("Coupe")
+                .setColor("Blue")
+                .setYear(2022)
+                .setFueltype("Premium Gas")
+                .build();
+    }
+
+    public cars constructTruck() {
+        return builder.setVehicletype("Truck")
+                .setColor("Black")
+                .setYear(2022)
+                .setFueltype("Diesel")
+                .build();
+    }
+
+}
