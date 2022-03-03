d4e5fb73fb7f29a502509c48e88ed6ca60ec63c1
rijabaig <90227392+rijabaig@users.noreply.github.com>
2022-03-03 16:07:32 -0500

    Update CarBuilderConc.java

--- CarBuilder/CarBuilderConc.java
+++ CarBuilder/CarBuilderConc.java
@@ -12,22 +12,22 @@ public class CarBuilderConc implements carbuilder {
     public cars build() {
         return car;
     }
-
+//setting color
     public carbuilder setColor(final String color) {
         car.setColor(color);
         return this;
     }
-
+//setting year made
     public carbuilder setYear(final int year) {
         car.setYear(year);
         return this;
     }
-
+//setting type of vehicle
     public carbuilder setVehicletype(final String vehicletype) {
         car.setVehicletype(vehicletype);
         return this;
     }
-
+//sets fueltyype of car
     public carbuilder setFueltype(final String fueltype) {
         car.setFueltype(fueltype);
         return this;
